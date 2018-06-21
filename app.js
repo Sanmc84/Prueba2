749fa15d5eae6c138d718e69ff6418b2afea38ee
Sandra\Sandy <moreloscs200884@gmail.com>
2018-06-21 12:17:21 -0500

    Prueba2

---
+++ app.js
@@ -0,0 +1,6 @@
+//preguntar temperatura en celsius
+var temperature= parseInt(prompt("¿Cual es la temperatura el celsius?"));
+//Convertir celsius a fahrenheit
+var convertToFahrenheit= (temperature * 1.8)+ 32;
+//mostrar resultado
+console.log("La temperatura en Fahrenheit es: "+ convertToFahrenheit);
